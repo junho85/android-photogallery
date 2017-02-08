48fe01b739ef1bc812760b835ae7a659e038c3f9
june.kim <june.kim@kakaocorp.com>
2017-02-08 09:06:45 +0900

    first commit

---
+++ app/src/main/java/kr/pe/junho85/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,12 @@
+package kr.pe.junho85.photogallery;
+
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
